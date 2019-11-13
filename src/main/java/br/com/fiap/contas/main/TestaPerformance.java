2bde933c9f791534b62ca7083f7a6a3b310edbe1
Jean Villete <jean.villete@gmail.com>
2019-11-12 23:16:29 -0300

    Teste de performance com HashSet; Tempo gasto em mili segundos; 12

--- src/main/java/br/com/fiap/contas/main/TestaPerformance.java
+++ src/main/java/br/com/fiap/contas/main/TestaPerformance.java
@@ -1,13 +1,13 @@
 package br.com.fiap.contas.main;
 
-import java.util.ArrayList;
 import java.util.Collection;
+import java.util.HashSet;
 
 public class TestaPerformance {
     public static void main(String[] args) {
-        System.out.println("Iniciando... Inserindo e pesquisando números com ArrayList");
+        System.out.println("Iniciando... Inserindo e pesquisando números com HashSet");
 
-        Collection<Integer> teste = new ArrayList<>();
+        Collection<Integer> teste = new HashSet<>();
         long inicio = System.currentTimeMillis();
 
         int total = 30_000;
