540e8ea5fa490aff7e06aa184bdff90f679f8a4c
Jean Villete <jean.villete@gmail.com>
2019-11-12 23:06:48 -0300

    Adição de List/LinkedList

--- src/main/java/br/com/fiap/contas/main/TestaLista.java
+++ src/main/java/br/com/fiap/contas/main/TestaLista.java
@@ -3,22 +3,19 @@ package br.com.fiap.contas.main;
 import br.com.fiap.contas.modelo.Conta;
 import br.com.fiap.contas.modelo.ContaCorrente;
 
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
-import java.util.Random;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class TestaLista {
     public static void main(String[] args) {
-        List<Conta> contas = Arrays.asList(
-                new ContaCorrente("pedro", 121, "4112"),
-                new ContaCorrente("paulo", 122, "4112"),
-                new ContaCorrente("artur", 124, "4112")
-        )
-                .stream()
-                .peek(conta -> conta.deposita(500 * new Random().nextDouble()))
-                .collect(Collectors.toList());
+        List<Conta> contas = new LinkedList<Conta>(){{
+            add( new ContaCorrente("pedro", 121, "4112") );
+            add( new ContaCorrente("paulo", 122, "4112") );
+            add( new ContaCorrente("artur", 124, "4112") );
+        }}
+            .stream()
+            .peek(conta -> conta.deposita(500 * new Random().nextDouble()))
+            .collect(Collectors.toList());
 
         Collections.sort(contas);
 
